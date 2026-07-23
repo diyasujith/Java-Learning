37ae4657a4462457d4184b9117b98683042cce3d
diyasujith <204838459+diyasujith@users.noreply.github.com>
2026-07-23 17:05:39 +0530

    Print Statement

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        int age = 27;
+        System.out.println("The number is "+age);
     }
 }
