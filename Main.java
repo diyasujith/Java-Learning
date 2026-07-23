5ce735234181e036979bb1f7cb42f26eae813fe5
diyasujith <204838459+diyasujith@users.noreply.github.com>
2026-07-23 18:28:42 +0530

    Formatted Strings

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String literalString1 = "abc";
-        String literalString2 = "abc";
-
-        String objectString1 = new String("xyz");
-        String objectString2 = new String("xyz");
-
-        System.out.println(literalString1 == literalString2);
-
-        System.out.println(objectString1 == objectString2);
-    }
+        //Formatted Strings are used to insert values into placeholders
+        String name = "Diya Sujith";
+        int age = 19;
+        double cgpa = 9.3;
+        System.out.println(String.format(
+            "My name is %s. I am %d years old. My cgpa is %f",name, age, cgpa));
+        }
 }
