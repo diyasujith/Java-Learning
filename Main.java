cac9fff79174f96bf5ddec50a6cd57f93a940fe7
diyasujith <204838459+diyasujith@users.noreply.github.com>
2026-07-24 15:42:40 +0530

    Switch Cases

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         System.out.print("Enter the first number: ");
         double num1 = scanner.nextDouble();
-        scanner.nextLine();
+        scanner.nextLine(); //Not required as next.Double() and next() automatically skip leading whitespeaces and newlines to find valid token
 
         System.out.print("Enter the second number: ");
         double num2 = scanner.nextDouble();
@@ -15,30 +15,38 @@ public class Main {
         System.out.print("What operation do you want to perform? ");
         char operation = scanner.next().charAt(0);
 
-        if (operation == '+') {
-            System.out.printf("%f + %f = %f", num1, num2, num1+num2);
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2; 
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+            
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero.");
+                    scanner.close(); //Closes scanner first before exiting main method
+                    return; //Exits main method immediately
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+
+            default:
+                System.out.printf("%c is not a supported operation.", operation);
+                scanner.close(); //Closes scanner first before exiting main method
+                return; //Exits main method immediately
         }
 
-        else if (operation == '-') {
-            System.out.printf("%f - %f = %f", num1, num2, num1-num2);
-        }
-
-        else if (operation == '*') {
-            System.out.printf("%f * %f = %f", num1, num2, num1*num2);
-        }
-
-        else if (operation == '/') {
-            if (num2 == 0) {
-                System.out.println("Cannot divide by 0");
-            }
-            else {
-                System.out.printf("%f / %f = %f", num1, num2, num1/num2);
-            }
-        }
-        
-        else {
-            System.out.printf("%c is not a supported operation.", operation);
-        }
+        System.out.printf("%f %c %f = %f", num1, operation, num2, result);
 
         scanner.close();
 
