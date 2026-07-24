60f141895ad643ff286d566baf2fc044de9dd9bf
diyasujith <204838459+diyasujith@users.noreply.github.com>
2026-07-24 16:47:25 +0530

    Accessing Elements in a Character Array

--- Main.java
+++ Main.java
@@ -1,54 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        char vowels[] = new char[5];
 
-        System.out.print("Enter the first number: ");
-        double num1 = scanner.nextDouble();
-        scanner.nextLine(); //Not required as next.Double() and next() automatically skip leading whitespeaces and newlines to find valid token
+        vowels[0] = 'a';
+        vowels[1] = 'e';
+        vowels[2] = 'i';
+        vowels[3] = 'o';
+        vowels[4] = 'u';
 
-        System.out.print("Enter the second number: ");
-        double num2 = scanner.nextDouble();
-        scanner.nextLine();
-
-        System.out.print("What operation do you want to perform? ");
-        char operation = scanner.next().charAt(0);
-
-        double result = 0;
-
-        switch (operation) {
-            case '+':
-                result = num1 + num2; 
-                break;
-
-            case '-':
-                result = num1 - num2;
-                break;
-            
-            case '*':
-                result = num1 * num2;
-                break;
-
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("Error: Division by zero.");
-                    scanner.close(); //Closes scanner first before exiting main method
-                    return; //Exits main method immediately
-                } else {
-                    result = num1 / num2;
-                }
-                break;
-
-            default:
-                System.out.printf("%c is not a supported operation.", operation);
-                scanner.close(); //Closes scanner first before exiting main method
-                return; //Exits main method immediately
-        }
-
-        System.out.printf("%f %c %f = %f", num1, operation, num2, result);
-
-        scanner.close();
+        System.out.println(vowels[2]);
 
     }
 }
